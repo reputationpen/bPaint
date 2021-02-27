6f2478076fc6a8f0bfe6b64d4926a9eba176b29b
Adam O'Deven <adam.odeven@gmail.com>
2021-02-27 13:58:01 -0500

    Test

---
+++ main.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg
+
+sg.theme('Dark Blue 3')  # please make your windows colorful
+
+layout = [[sg.Text('Persistent window')],
+          [sg.Input()],
+          [sg.Button('Read'), sg.Exit()],
+          [sg.Button('Pain')]]
+
+window = sg.Window('Window that stays open', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    print(event, values)
+
+window.close()
