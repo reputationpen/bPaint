d0575a9b2cfba7b9e3a3249dc28252f6974788a1
Adam O'Deven <adam.odeven@gmail.com>
2021-02-28 21:38:13 -0500

    Test 4

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import PySimpleGUI as sg
 sg.theme('Dark Blue 3')  # please make your windows colorful
 
 layout = [[sg.Text('Pretty cool')],
-          [sg.Input('What if i put text here')],
-          [sg.Button('Read'), sg.Exit()],
-          [sg.Button('More Changes')]]
+          [sg.Input()],
+          [sg.Button('Read')]]
 
 window = sg.Window('Bro I made changes', layout)
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    if event == sg.WIN_CLOSED:
         break
     print(event, values)
 
